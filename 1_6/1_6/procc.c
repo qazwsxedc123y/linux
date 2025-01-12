9858d3d1dfbe751e7819d10031efbc5e44d4a1aa
qazwsxedc123y <1476303287@qq.com>
2025-01-12 21:49:15 +0800

    基础io

--- 1_6/1_6/procc.c
+++ 1_6/1_6/procc.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
-
 int main()
 {
-    int fd = open("test.txt", O_RDONLY);
-    // 文件操作代码...
-    printf("%d\n",fd);
-    close(fd);
-    return 0;
+	int fd = open("log.txt", O_WRONLY | O_CREAT, 0666);
+	if (fd < 0){
+		perror("open");
+		return 1;
+	}
+	close(1);
+	dup2(fd, 1);
+	printf("hello printf\n");
+	fprintf(stdout, "hello fprintf\n");
+	return 0;
 }
