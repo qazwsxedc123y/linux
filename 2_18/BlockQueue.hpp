9ada94708bf7aef138a27dc4bdb9ecbc3f20c91e
simple-and-silly-matcha-c <1476303287@qq.com>
2025-02-19 23:06:01 +0800

    生产消费模型

--- 2_18/BlockQueue.hpp
+++ 2_18/BlockQueue.hpp
@@ -1,5 +1,77 @@
 #pragma once
-
 #include <iostream>
 #include <queue>
 #include <pthread.h>
+
+// 设置一个锁 两个条件变量
+
+template <class T>
+class BlockQueue
+{
+    static const int defalutnum = 20;
+public:
+    BlockQueue(int maxcap = defalutnum):maxcap_(maxcap)
+    {
+        pthread_mutex_init(&mutex_, nullptr);
+        pthread_cond_init(&c_cond_, nullptr);
+        pthread_cond_init(&p_cond_, nullptr);
+        // low_water_ = maxcap_/3;
+        // high_water_ = (maxcap_*2)/3;
+    }
+    bool isFull()
+    {
+        return q_.size() == maxcap_;
+    }
+    bool isEmpty()
+    {
+        return q_.size() == 0;
+    }
+    void push(const T &in)
+    {
+        pthread_mutex_lock(&mutex_);
+        // 插入的前提是：队列不满
+        while(isFull()) // 如果满则等待
+        {
+            pthread_cond_wait(&p_cond_, &mutex_); //1. 调用的时候，自动释放锁 2.？
+        }
+        // 不满，插入
+        q_.push(in); // 生产完后，要记得唤醒在Empty条件变量下等待的消费者线程
+        // if(q_.size() > high_water_) pthread_cond_signal(&c_cond_);
+        pthread_cond_signal(&c_cond_);
+        pthread_mutex_unlock(&mutex_);
+
+    }
+    T pop()
+    {
+        pthread_mutex_lock(&mutex_);
+        // 删除的前提是：队列不为空
+        while(isEmpty())
+        {
+            pthread_cond_wait(&c_cond_, &mutex_);
+        }
+        T out = q_.front();
+        q_.pop();
+        // if(q_.size()<low_water_) pthread_cond_signal(&p_cond_)
+        pthread_cond_signal(&p_cond_);
+        pthread_mutex_unlock(&mutex_);
+
+        return out;
+    }
+    ~BlockQueue()
+    {
+        pthread_mutex_destroy(&mutex_);
+        pthread_cond_destroy(&c_cond_);
+        pthread_cond_destroy(&p_cond_);
+    }
+private:
+    std::queue<T> q_; // 共享资源, q被当做整体使用的，q只有一份，加锁。但是共享资源也可以被看做多份！
+
+    int maxcap_;      // 极值
+    pthread_mutex_t mutex_;
+    pthread_cond_t c_cond_; // 消费
+    pthread_cond_t p_cond_; // 生产
+
+
+    int low_water_;
+    int high_water_;
+};
--- 2_18/main.cc
+++ 2_18/main.cc
@@ -1,34 +1,69 @@
 #include <unistd.h>
 #include <ctime>
-#include <iostream>
 #include "BlockQueue.hpp"
+#include "Task.hpp"
 
-using namespace std;
-
-// 生产消费模型  
+// 生产消费模型
 // 3个生产者  5个消费者
+
+// 加上Task 版
 
 void *Consumer(void *args)
 {
+    BlockQueue<Task> *bq = static_cast<BlockQueue<Task> *>(args);
+    while (true)
+    {
+        // 消费
+        Task t = bq->pop();
+        // 计算
+        // t.run();
+        t();
+        
+        std::cout << "处理任务: " << t.GetTask() << " 运算结果是： " << t.GetResult() << " thread id: " << pthread_self() << std::endl;
 
+        sleep(1);
+    }
 }
 
 void *Productor(void *args)
 {
+    int len = opers.size();
+    BlockQueue<Task> *bq = static_cast<BlockQueue<Task> *>(args);
+    int x = 10;
+    int y = 20;
+    while (true)
+    {
+        // 模拟生产者生产数据
+        int data1 = rand() % 10 + 1; // [1,10]
+        usleep(10);
+        int data2 = rand() % 10;
+        char op = opers[rand() % len];
+        Task t(data1, data2, op);
 
+        // 生产
+        bq->push(t);
+        std::cout << "生产了一个任务: " << t.GetTask() << " thread id: " << pthread_self() << std::endl;
+        sleep(1);
+    }
 }
 
 int main()
 {
+    srand(time(nullptr));
+
+    // 因为 321 原则
+    // BlockQueue 内部可不可以传递其他数据，比如对象？比如任务？？？
+    BlockQueue<int> *bq = new BlockQueue<int>(5);
+
     pthread_t c[3], p[5];
-    for(int i = 0;i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        pthread_create(c + i, nullptr, Consumer, nullptr);
+        pthread_create(c + i, nullptr, Consumer, bq);
     }
 
     for (int i = 0; i < 5; i++)
     {
-        pthread_create(p + i, nullptr, Productor, nullptr);
+        pthread_create(p + i, nullptr, Productor, bq);
     }
 
     for (int i = 0; i < 3; i++)
@@ -42,3 +77,63 @@ int main()
     }
     return 0;
 }
+
+// 普通版
+
+// void *Consumer(void *args)
+// {
+//     BlockQueue<int> *bq = static_cast<BlockQueue<int> *>(args);
+//     while(true)
+//     {
+//         int tmp = rand() % 100;
+//         bq->push(tmp);
+//         std::cout << "生产了一个任务" << "，任务为 : " << tmp << std::endl;
+//         // std::cout << "处理任务: " <<  t.GetTask()  << " 运算结果是： " << t.GetResult() << " thread id: " << pthread_self() << std::endl;
+
+//         sleep(1);
+//     }
+// }
+
+// void *Productor(void *args)
+// {
+
+//     BlockQueue<int> *bq = static_cast<BlockQueue<int> *>(args);
+//     while(true)
+//     {
+//         int tmp = bq->pop();
+//         std::cout << "消费了一个任务" << "，任务为 : " << tmp << std::endl;
+//         // std::cout << "生产了一个任务: " << t.GetTask() << " thread id: " << pthread_self() << std::endl;
+
+//     }
+// }
+
+// int main()
+// {
+//     srand(time(nullptr));
+
+//     // 因为 321 原则
+//     // BlockQueue 内部可不可以传递其他数据，比如对象？比如任务？？？
+//     BlockQueue<int> *bq = new BlockQueue<int>(5);
+
+//     pthread_t c[3], p[5];
+//     for(int i = 0;i < 3; i++)
+//     {
+//         pthread_create(c + i, nullptr, Consumer, bq);
+//     }
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         pthread_create(p + i, nullptr, Productor, bq);
+//     }
+
+//     for (int i = 0; i < 3; i++)
+//     {
+//         pthread_join(c[i], nullptr);
+//     }
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         pthread_join(p[i], nullptr);
+//     }
+//     return 0;
+// }
