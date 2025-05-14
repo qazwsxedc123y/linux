606413016da93fa1ca5dac16cd98857d4a6b3e12
simple-and-silly-matcha-c <1476303287@qq.com>
2025-05-14 21:53:27 +0800

    http

---
+++ http/wwwroot/HttpServer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <pthread.h>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unordered_map>
+
+
+#include "Socket.hpp"
+#include "Log.hpp"
+
+class ThreadData
+{
+
+};
+
+class HttpRequest
+{
+
+};
+
+class HttpServer
+{
+
+};
